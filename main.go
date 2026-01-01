18f1a12dd89691fd6b555f15faaeb5ef3421f8cc
Harsh Kumar <jhah1225@gmail.com>
2026-01-01 21:02:09 +0530

    Add pizza app rating feature
    Implement basic pizza rating functionality with input handling and type conversion.

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+func main() {
+	fmt.Println("welcome to our pizza app")
+	fmt.Println("please rate our pizza between 1 and  5")
+
+	reader := bufio.NewReader(os.Stdin)
+
+    input, _ :=	reader.ReadString('\n')
+
+	fmt.Println("thanks for ypurs rating ", input)
+
+	fmt.Printf("type of input is. : %T \n",input)
+
+
+	//type conversion
+	numRating, err := strconv.ParseFloat(strings.TrimSpace(input) , 64)
+
+	if err != nil {
+		fmt.Println("error in conversion" , err)
+	}else {
+		fmt.Println("added 1 to your rating " , numRating+1)
+
+	}
+
+
+}
+ 
